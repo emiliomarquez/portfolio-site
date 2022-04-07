33fe82cda66e00febd98afd15bb77736d2e37a8d
Emilio Marquez Fernandez <44658981+emiliomarquez@users.noreply.github.com>
2022-04-06 23:04:37 -0700

    first commit 2.0

---
+++ src/main.js
@@ -0,0 +1,9 @@
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://personal-site-1662c-default-rtdb.firebaseio.com"
+});
