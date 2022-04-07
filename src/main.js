6209246add98c94751a977e03cb5dce09cd8940e
Emilio Marquez Fernandez <44658981+emiliomarquez@users.noreply.github.com>
2022-04-06 23:15:48 -0700

    removed junk files

--- src/main.js
+++
@@ -1,9 +0,0 @@
-
-var admin = require("firebase-admin");
-
-var serviceAccount = require("path/to/serviceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://personal-site-1662c-default-rtdb.firebaseio.com"
-});
